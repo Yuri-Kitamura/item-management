9bf5c3cd06a5226ca28d3211f233e65c93214246
yuri kitamura <118658450+Yuri-Kitamura@users.noreply.github.com>
2023-04-16 20:36:08 +0900

    crud機能の追加

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Item;
+use Illuminate\Support\Facades\Gate;
 
 class ItemController extends Controller
 {
@@ -21,15 +22,28 @@ class ItemController extends Controller
     /**
      * 商品一覧
      */
-    public function index()
+    public function index(Request $request)
     {
         // 商品一覧取得
         $items = Item
             ::where('items.status', 'active')
-            ->select()
-            ->get();
+            ->select();
 
-        return view('item.index', compact('items'));
+        // キーワード検索処理
+        $keyword = $request->input('keyword');
+
+        //$keywordが空ではない場合、検索処理を実行
+        if (!empty($keyword)) {
+            $items->where('name', 'LIKE', "%{$keyword}%")
+                ->orWhere('id', 'LIKE', "%{$keyword}%")
+                ->orWhere('type', 'LIKE', "%{$keyword}%")
+                ->orWhere('detail', 'LIKE', "%{$keyword}%");
+        }
+
+        /* ページネーション */
+        $items = $items->orderBy('id', 'desc')->paginate(20);
+        //$items = $items->orderBy('id', 'desc')->get();
+        return view('item.index', ['items' => $items]);
     }
 
     /**
@@ -37,24 +51,65 @@ class ItemController extends Controller
      */
     public function add(Request $request)
     {
-        // POSTリクエストのとき
-        if ($request->isMethod('post')) {
-            // バリデーション
-            $this->validate($request, [
-                'name' => 'required|max:100',
-            ]);
+        // 管理者だけ商品登録ができる
+        if (Gate::allows('admin')) {
+            // POSTリクエストのとき
+            if ($request->isMethod('post')) {
+                // バリデーション
+                $this->validate($request, [
+                    'name' => 'required|max:100',
+                ]);
 
-            // 商品登録
-            Item::create([
-                'user_id' => Auth::user()->id,
-                'name' => $request->name,
-                'type' => $request->type,
-                'detail' => $request->detail,
-            ]);
+                // 商品登録
+                Item::create([
+                    'user_id' => Auth::user()->id,
+                    'name' => $request->name,
+                    'type' => $request->type,
+                    'detail' => $request->detail,
+                ]);
 
-            return redirect('/items');
+                return redirect('/items');
+            }
+            return view('item.add');
         }
+    }
 
-        return view('item.add');
+    /**
+     * 商品を編集
+     */
+    public function edit(Item $item)
+    {
+        if (Gate::allows('admin')) {
+            return view('item.edit', compact('item'));
+        }
+    }
+
+    /**
+     * 商品を更新
+     */
+
+    public function update(Request $request, Item $item)
+    {
+        $request->validate([
+            'name' => 'required|max:100',
+            'type' => 'required|max:255',
+            'detail' => 'nullable|max:500',
+        ]);
+
+        if (Gate::allows('admin')) {
+            $item->update($request->all());
+            return redirect()->route('item.index')->with('success', 'Item updated successfully.');
+        }
+    }
+
+    /**
+     * 商品を削除
+     */
+    public function destroy(Item $item)
+    {
+        if (Gate::allows('admin')) {
+            $item->delete();
+            return redirect()->route('item.index')->with('success', 'Item deleted successfully.');
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+// use App\Http\Controllers\ItemController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,7 +23,12 @@ Auth::routes();
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::prefix('items')->group(function () {
-    Route::get('/', [App\Http\Controllers\ItemController::class, 'index']);
-    Route::get('/add', [App\Http\Controllers\ItemController::class, 'add']);
-    Route::post('/add', [App\Http\Controllers\ItemController::class, 'add']);
+    Route::get('/', [App\Http\Controllers\ItemController::class, 'index'])->name('item.index');
+    Route::group(['middleware' => ['can:admin']], function () {
+        Route::get('/add', [App\Http\Controllers\ItemController::class, 'add'])->name('item.add');
+        Route::post('/add', [App\Http\Controllers\ItemController::class, 'add']);
+        Route::get('/{item}/edit', [App\Http\Controllers\ItemController::class, 'edit'])->name('item.edit');
+        Route::post('/{item}/update', [App\Http\Controllers\ItemController::class, 'update'])->name('item.update');
+        Route::delete('/{item}/delete', [App\Http\Controllers\ItemController::class, 'destroy'])->name('item.destroy');
+    });
 });
